33b75c7338d83e88405988bb8bd20af8a023b44c
ITSid30 <85026170+ITSid30@users.noreply.github.com>
2021-10-13 11:41:08 +0530

    Update Sem3Ass1bPrimeNos.c

--- Sem3Ass1bPrimeNos.c
+++ Sem3Ass1bPrimeNos.c
@@ -1,13 +1,8 @@
 #include <stdio.h>
 
-int main(void) {
-  int n1,n2, count;
-  printf("\nEnter the Start : \n");
-  scanf("%d", &n1);
-  printf("\nEnter the End : \n");
-  scanf("%d", &n2);
-  printf("\nThe Prime Numbers Between %d and %d are :\n", n1,n2);
-  for(int i=n1;i<=n2;i++)
+int primenos(int a, int b) {
+  int count;
+  for(int i=a;i<=b;i++)
   {
     count =0;
     for(int j=1;j<=i;j++)
@@ -22,6 +17,19 @@ int main(void) {
       printf("%d\n", i);
     }
   }
+  return 0;
+}
+
+int main(void) {
+  int n1,n2, count;
+  printf("\nEnter the Start : \n");
+  scanf("%d", &n1);
+  printf("\nEnter the End : \n");
+  scanf("%d", &n2);
+  printf("\nThe Prime Numbers Between %d and %d are :\n", n1,n2);
+  
+  primenos(n1, n2);
+
   printf("\n");
   return 0;
 }
